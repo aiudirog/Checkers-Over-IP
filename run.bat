d4b1b68cc91c5557663157e7420293a8b9df1c07
rmaiudi <rmaiudi@gmail.com>
2015-03-16 23:56:16 -0400

    I am going to shoot this thing

--- run.bat
+++ run.bat
@@ -3,6 +3,7 @@ chdir /d "C:\Program Files\Checkers-Over-IP"
 ECHO Updating local repo....
 git fetch
 ECHO Checking for differences....
+set COUNT=None
 for /f "delims=" %%a in ('git diff --numstat HEAD origin/master') do @set COUNT=%%a
 set _count_=%COUNT:~0,1%
 ECHO %COUNT%
