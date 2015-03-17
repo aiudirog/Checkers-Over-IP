c9c08ec2b7fd97e3a3d7860c618033e5bfcaac09
rmaiudi <rmaiudi@gmail.com>
2015-03-17 00:15:26 -0400

    test

--- run.bat
+++ run.bat
@@ -3,7 +3,6 @@ chdir /d "C:\Program Files\Checkers-Over-IP"
 ECHO Updating local repo....
 git fetch
 ECHO Checking for differences....
-
 set COUNT=
 for /f "tokens=*" %%a in ('git diff --numstat HEAD origin/master') do @set COUNT=%%a
 if '%COUNT%' == '' (echo No need to pull) else (echo Update needed)
