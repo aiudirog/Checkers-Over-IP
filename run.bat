05f6bb9a993485b7783db3a1abe6ed674cf163c6
rmaiudi <rmaiudi@gmail.com>
2015-03-16 23:10:04 -0400

    ditto

--- run.bat
+++ run.bat
@@ -3,9 +3,7 @@ chdir /d "C:\Program Files\Checkers-Over-IP"
 ECHO Updating local repo....
 git fetch
 ECHO Checking for differences....
-set COUNT=
 for /f "delims=" %%a in ('git rev-list HEAD...origin/master --count') do @set COUNT=%%a
-set COUNT
 ECHO %COUNT%
 
 timeout /T 15
