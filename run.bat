8cc7ee880254941cf19959f0fe78966b9e7cf3e0
rmaiudi <rmaiudi@gmail.com>
2015-03-16 23:44:57 -0400

    hmmm

--- run.bat
+++ run.bat
@@ -6,7 +6,7 @@ ECHO Checking for differences....
 for /f "delims=" %%a in ('git diff --numstat HEAD origin/master') do @set COUNT=%%a
 set _count_ = %COUNT:~0,2%
 ECHO %COUNT%
-ECHO '%_count_%'
+ECHO %_count_%
 if '%_count_%' == '' (echo No need to pull) else (echo Update needed)
 TIMEOUT /T 15
 if '%_count_%' == '' (goto runWithoutPull) else (goto runWithPull)
