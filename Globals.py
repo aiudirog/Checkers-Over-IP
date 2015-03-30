767507ea5800b0fdfde8f849efaae4640bac5a43
rmaiudi <rmaiudi@gmail.com>
2015-03-30 10:20:38 -0400

    More cleaning

--- Globals.py
+++ Globals.py
@@ -4,12 +4,13 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
 from platform import system
+import getpass
 
 Graphics = os.path.join(os.getcwd(),"Graphics")
 System = system()
-if System == "Linux": basePath = "~/.Checkers-Over-IP"
+if System == "Linux": basePath = "/home/{}/.Checkers-Over-IP".format(getpass.getuser())
 elif System == "Windows": basePath = os.path.join(os.getenv('APPDATA'),".Checkers-Over-IP")
-else: basePath = "~/.Checkers-Over-IP"
+else: basePath = "/home/{}/.Checkers-Over-IP".format(getpass.getuser())
 LastConnectionFile = os.path.join(basePath,"LastConnection.dat")
 NoInternet = False
 try: myIP =  urllib.request.urlopen('http://bot.whatismyipaddress.com').read().decode(encoding='UTF-8')
